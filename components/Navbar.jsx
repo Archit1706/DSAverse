a18d0fc499b80839399668fdefdc3976aad5af62
Archit1706 <architrathod77@gmail.com>
2025-06-23 23:25:04 -0500

    update navbar component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState, useEffect } from 'react';
 import { algorithmCategories } from '../data/algorithmCategories';
 import { ChevronDown, Menu, X } from 'lucide-react';
